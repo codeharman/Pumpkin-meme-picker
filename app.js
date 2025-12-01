1f97c8d4de98bd15bfdc74f2a65bef18bb90900e
Harmanjot Singh <161106030+codeharman@users.noreply.github.com>
2025-12-01 22:02:13 +0530

    made the logic

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 import { catsData } from './data.js';
+const header = document.querySelector('.header');
 
-console.log(catsData)
+let preview = ''
 
-// for(let i of catsData) {
-//   console.log(i)
-// }
+for(let i of catsData) {
+  console.log(i)
+  for(let emotion of i.emotionTags)
+    if(!preview.includes(emotion)){
+      preview += `
+      <div class='radio'>
+        <input
+          type='radio'
+          value='${emotion}'
+          id='${emotion}'
+          name='choice'
+        >
+        <label for='choice'>${emotion}</label>
+      </div>
+        `
+    }
+}
+
+header.innerHTML = preview
+
+console.log(header)
